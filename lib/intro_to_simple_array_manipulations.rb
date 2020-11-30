aba800e4c69ff3404afa62ec93cec5405c443cf2
Patrick Tuszakowski <mrtall500@gmail.com>
2020-11-30 21:25:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,8 @@ def using_concat(array_1, array_2)
   array_1.concat(array_2)
 end
 
-def using_insert(array, element)
+def using_insert(array, element
+  binding.pry
   array.insert(element)
-binding.pry
+
 end
